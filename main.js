c185044c370e56f459a5c4b88e1f52c38255b927
semperprimum <bkeem0266+github@gmail.com>
2023-10-05 11:02:40 +0600

    minor fixes

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const docxFileInput = document.getElementById("docxFileInput");
-const convertButton = document.getElementById("convertButton");
-const downloadLink = document.getElementById("downloadLink");
+document.addEventListener("DOMContentLoaded", () => {
+  const docxFileInput = document.getElementById("docxFileInput");
+  const convertButton = document.getElementById("convertButton");
+  const downloadLink = document.getElementById("downloadLink");
 
-convertButton.addEventListener("click", function () {
-  const file = docxFileInput.files[0];
+  convertButton.addEventListener("click", function () {
+    const file = docxFileInput.files[0];
 
-  if (!file) {
-    alert("Please select a .docx file.");
-    return;
-  }
+    if (!file) {
+      alert("Please select a .docx file.");
+      return;
+    }
 
-  const reader = new FileReader();
+    const reader = new FileReader();
 
-  reader.onload = function (event) {
-    const docxData = event.target.result;
+    reader.onload = function (event) {
+      const docxData = event.target.result;
 
-    mammoth
-      .extractRawText({ arrayBuffer: docxData })
-      .then(function (result) {
-        const text = result.value;
+      mammoth
+        .extractRawText({ arrayBuffer: docxData })
+        .then(function (result) {
+          const text = result.value;
 
-        const blob = new Blob([text], { type: "text/plain" });
+          const blob = new Blob([text], { type: "text/plain" });
 
-        const downloadUrl = URL.createObjectURL(blob);
-        downloadLink.href = downloadUrl;
-        downloadLink.download = file.name.replace(".docx", ".txt");
-        downloadLink.style.display = "block";
-      })
-      .catch(function (error) {
-        console.error("Error converting .docx to text:", error);
-      });
-  };
+          const downloadUrl = URL.createObjectURL(blob);
+          downloadLink.href = downloadUrl;
+          downloadLink.download = file.name.replace(".docx", ".txt");
+          downloadLink.style.display = "block";
+        })
+        .catch(function (error) {
+          console.error("Error converting .docx to text:", error);
+        });
+    };
 
-  reader.readAsArrayBuffer(file);
+    reader.readAsArrayBuffer(file);
+  });
 });
